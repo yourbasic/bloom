08eaa09679f7ac338ece5b667780d6e20e2a80d0
korthaj <korthaj@users.noreply.github.com>
2017-05-07 14:17:59 +0200

    Add false positives example

--- example_test.go
+++ example_test.go
@@ -3,10 +3,12 @@ package bloom_test
 import (
 	"fmt"
 	"github.com/yourbasic/bloom"
+	"math/rand"
+	"strconv"
 )
 
 // Create and use a Bloom filter.
-func Example() {
+func Example_basics() {
 	// Create a Bloom filter with room for 10000 elements
 	// at a false-positives rate less than 0.5 percent.
 	blacklist := bloom.New(10000, 200)
@@ -22,4 +24,29 @@ func Example() {
 		fmt.Println(url, "has not yet been added to our blacklist.")
 	}
 	// Output: https://rascal.com seems to be shady.
+}
+
+// Count the number of false positives.
+func Example_falsePositives() {
+	// Create a Bloom filter with room for n elements
+	// at a false-positives rate less than 1/p.
+	n := 1000
+	p := 100
+	filter := bloom.New(n, p)
+
+	// Add n random strings.
+	for i := 0; i < n; i++ {
+		filter.Add(strconv.FormatUint(rand.Uint64(), 10))
+	}
+
+	// Do n random lookups and count the (mostly accidental) hits.
+	// It shouldn't be much more than n/p, and hopefully less.
+	count := 0
+	for i := 0; i < n; i++ {
+		if filter.Test(strconv.FormatUint(rand.Uint64(), 10)) {
+			count++
+		}
+	}
+	fmt.Println(count, "mistakes were made.")
+	// Output: 1 mistakes were made.
 }
